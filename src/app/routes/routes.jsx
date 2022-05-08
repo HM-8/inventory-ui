38a641d01b6ee88edf5a2fd75061d0f60f0cfa57
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-05-08 13:09:16 +0300

    Fixed redirection

--- src/app/routes/routes.jsx
+++ src/app/routes/routes.jsx
@@ -10,17 +10,18 @@ import attendanceRoutes from 'app/views/attendance/AttendanceRoutes'
 import employeeRoutes from 'app/views/employee/EmployeeRoutes'
 import leaveRoutes from 'app/views/leave/LeaveRoutes'
 import reportRoutes from 'app/views/report/ReportRoutes'
+import defaultRoutes from 'app/views/default/DefaultRoutes'
 
 export const AllPages = () => {
     const all_routes = [
         {
             element: <MatxLayout />,
-            children: [...dashboardRoutes, ...chartsRoute, ...materialRoutes, ...attendanceRoutes, ...employeeRoutes, ...leaveRoutes, ...reportRoutes],
+            children: [...dashboardRoutes, ...chartsRoute, ...materialRoutes, ...attendanceRoutes, ...employeeRoutes, ...leaveRoutes, ...reportRoutes, ...defaultRoutes],
         },
         ...sessionRoutes,
         {
             path: '/',
-            element: <Navigate to="dashboard/default" />,
+            element: <Navigate to="dashboard" />,
         },
         {
             path: '*',
