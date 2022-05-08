6baabaf75280dd216e3334436d34ec2ea165ece3
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-05-08 03:26:21 +0300

    Updated table component

--- src/app/views/material-kit/tables/PaginationTable.jsx
+++ src/app/views/material-kit/tables/PaginationTable.jsx
@@ -1,15 +1,14 @@
 import {
-    IconButton,
     Table,
     TableHead,
     TableBody,
     TableRow,
     TableCell,
-    Icon,
     TablePagination,
 } from '@mui/material'
 import React from 'react'
 import { Box, styled } from '@mui/system'
+
 
 const StyledTable = styled(Table)(({ theme }) => ({
     whiteSpace: 'pre',
@@ -31,73 +30,7 @@ const StyledTable = styled(Table)(({ theme }) => ({
     },
 }))
 
-const subscribarList = [
-    {
-        name: 'john doe',
-        date: '18 january, 2019',
-        amount: 1000,
-        status: 'close',
-        company: 'ABC Fintech LTD.',
-    },
-    {
-        name: 'kessy bryan',
-        date: '10 january, 2019',
-        amount: 9000,
-        status: 'open',
-        company: 'My Fintech LTD.',
-    },
-    {
-        name: 'kessy bryan',
-        date: '10 january, 2019',
-        amount: 9000,
-        status: 'open',
-        company: 'My Fintech LTD.',
-    },
-    {
-        name: 'james cassegne',
-        date: '8 january, 2019',
-        amount: 5000,
-        status: 'close',
-        company: 'Collboy Tech LTD.',
-    },
-    {
-        name: 'lucy brown',
-        date: '1 january, 2019',
-        amount: 89000,
-        status: 'open',
-        company: 'ABC Fintech LTD.',
-    },
-    {
-        name: 'lucy brown',
-        date: '1 january, 2019',
-        amount: 89000,
-        status: 'open',
-        company: 'ABC Fintech LTD.',
-    },
-    {
-        name: 'lucy brown',
-        date: '1 january, 2019',
-        amount: 89000,
-        status: 'open',
-        company: 'ABC Fintech LTD.',
-    },
-    {
-        name: 'lucy brown',
-        date: '1 january, 2019',
-        amount: 89000,
-        status: 'open',
-        company: 'ABC Fintech LTD.',
-    },
-    {
-        name: 'lucy brown',
-        date: '1 january, 2019',
-        amount: 89000,
-        status: 'open',
-        company: 'ABC Fintech LTD.',
-    },
-]
-
-const PaginationTable = () => {
+const PaginationTable = ({ columns, rows, ...otherProps }) => {
     const [rowsPerPage, setRowsPerPage] = React.useState(5)
     const [page, setPage] = React.useState(0)
 
@@ -115,38 +48,42 @@ const PaginationTable = () => {
             <StyledTable>
                 <TableHead>
                     <TableRow>
-                        <TableCell>Name</TableCell>
-                        <TableCell>Company</TableCell>
-                        <TableCell>Start Date</TableCell>
-                        <TableCell>Status</TableCell>
-                        <TableCell>Amount</TableCell>
-                        <TableCell>Action</TableCell>
+                        {columns.map((column) => (
+                            <TableCell
+                                key={column.id}
+                            >
+                                {column.label}
+                            </TableCell>
+                        ))}
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {subscribarList
+                    {rows
                         .slice(
                             page * rowsPerPage,
                             page * rowsPerPage + rowsPerPage
                         )
-                        .map((subscriber, index) => (
-                            <TableRow key={index}>
-                                <TableCell align="left">
-                                    {subscriber.name}
-                                </TableCell>
-                                <TableCell align="left">
-                                    {subscriber.company}
-                                </TableCell>
-                                <TableCell align="left">
-                                    {subscriber.date}
-                                </TableCell>
-                                <TableCell>{subscriber.status}</TableCell>
-                                <TableCell>${subscriber.amount}</TableCell>
-                                <TableCell>
-                                    <IconButton>
-                                        <Icon color="error">close</Icon>
-                                    </IconButton>
-                                </TableCell>
+                        .map((row, index) => (
+                            <TableRow
+                                hover
+                                role="checkbox"
+                                tabIndex={-1}
+                                key={row.allocation}
+                            >
+                                {columns.map((column) => {
+                                    const value = row[column.id]
+                                    return (
+                                        <TableCell
+                                            key={column.id}
+                                            align={column.align}
+                                        >
+                                            {column.format &&
+                                            typeof value === 'number'
+                                                ? column.format(value)
+                                                : value}
+                                        </TableCell>
+                                    )
+                                })}
                             </TableRow>
                         ))}
                 </TableBody>
@@ -156,7 +93,7 @@ const PaginationTable = () => {
                 sx={{ px: 2 }}
                 rowsPerPageOptions={[5, 10, 25]}
                 component="div"
-                count={subscribarList.length}
+                count={rows.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 backIconButtonProps={{
