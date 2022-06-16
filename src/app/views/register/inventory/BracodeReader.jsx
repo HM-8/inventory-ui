477b54151724c52e53fb81dd0cc2d7d27ccd7265
eze-une <mruniverse1x@gmail.com>
2022-06-16 11:50:08 +0300

    working barcode code scanner code

--- src/app/views/register/inventory/BracodeReader.jsx
+++ src/app/views/register/inventory/BracodeReader.jsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, styled } from '@mui/system'
 import { Button } from '@mui/material'
+
+import BarcodeScannerComponent from 'react-qr-barcode-scanner'
+
 function BracodeReader() {
     const StyledButton = styled(Button)(({ theme }) => ({
         margin: theme.spacing(1),
     }))
+    const [data, setData] = useState('Not Found')
+    const [stopStream, setStopStream] = useState(false)
+
     return (
-        <div>
-            <StyledButton variant="contained" color="secondary">
-                Barcode
-            </StyledButton>
-        </div>
+        <>
+            {stopStream ? (
+                <BarcodeScannerComponent
+                    width={500}
+                    height={500}
+                    stopStream={stopStream}
+                    onUpdate={(err, result) => {
+                        if (result) {
+                            setData(result.text)
+                            setStopStream(true)
+                        } else {
+                            setData('Not Found')
+                        }
+                    }}
+                />
+            ) : (
+                'Nothing to show here'
+            )}
+
+            <p>{data}</p>
+            <button onClick={() => setStopStream(true)}>Scan</button>
+            <button onClick={() => setStopStream(false)}>Cancel</button>
+        </>
     )
 }
 
