5ee584ef15a2544509a84bfd5a973139397ca4b9
eze-une <mruniverse1x@gmail.com>
2022-06-20 15:59:38 +0300

    added table

--- src/app/views/report/hr/Attendance.jsx
+++ src/app/views/report/hr/Attendance.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PaginationTable from '../../material-kit/tables/PaginationTable'
+import { Breadcrumb, SimpleCard } from 'app/components'
+import { Box, styled } from '@mui/system'
+import { Grid, Button } from '@mui/material'
+import data from '../../../utils/data/employmentType.json'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import TableButton from '../../material-kit/buttons/LinkButton'
 
-function Attendance() {
-  return (
-    <div>Attendance</div>
-  )
+const Container = styled('div')(({ theme }) => ({
+    margin: '30px',
+    [theme.breakpoints.down('sm')]: {
+        margin: '16px',
+    },
+    '& .breadcrumb': {
+        marginBottom: '30px',
+        [theme.breakpoints.down('sm')]: {
+            marginBottom: '16px',
+        },
+    },
+}))
+
+const columns = [
+  { id: 'employee', label: 'Employee', minWidth: 10 },
+    { id: 'date', label: 'Date', minWidth: 10 },
+    { id: 'status', label: 'Status ', minWidth: 10 },
+    { id: 'shiftStart', label: 'Shift Start Time', minWidth: 10 },
+    { id: 'shiftEnd', label: 'Shift End Time', minWidth: 10 },
+  ]
+
+function createData(shiftStart, date, shiftEnd,employee) {
+    return {
+        shiftStart: shiftStart,
+        date: date,
+        shiftEnd: shiftEnd,
+        employee:employee,
+    }
 }
 
-export default Attendance
+const rows = data.map((item, index) => {
+    const container = {}
+    createData(
+        (container.shiftStart = item.shiftStart),
+        (container.date = item.date),
+        (container.shiftEnd = item.shiftEnd),
+        (container.employee = item.employee),
+    )
+    return container
+})
+
+const StyledButton = styled(Button)(({ theme }) => ({
+    margin: theme.spacing(1),
+}))
+
+const AttendnaceReport = () => {
+    const [show, setShow] = useState(false)
+
+    const handleButton = () => {
+        setShow(!show)
+        console.log(show)
+    }
+    return (
+        <Container>
+            <div className="breadcrumb">
+                <Breadcrumb
+                    routeSegments={[
+                        { name: 'Report', path: '/dashboard' },
+                        { name: 'Attendance' },
+                    ]}
+                />
+            </div>
+            <Box py="1px" />
+            <Grid
+                container
+                direction="row"
+                spacing={2}
+                justifyContent="flex-end"
+            >
+            </Grid>
+            <Grid item xs={12}>
+                <SimpleCard title="Attendance">
+                    <PaginationTable columns={columns} rows={rows} />
+                </SimpleCard>
+            </Grid>
+        </Container>
+    )
+}
+
+export default AttendnaceReport
--- src/app/views/report/hr/Termination.jsx
+++ src/app/views/report/hr/Termination.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import PaginationTable from '../../material-kit/tables/PaginationTable'
+import { Breadcrumb, SimpleCard } from 'app/components'
+import { Box, styled } from '@mui/system'
+import { Grid, Button } from '@mui/material'
+import data from '../../../utils/data/employmentType.json'
+import EditIcon from '@mui/icons-material/Edit'
+import DeleteIcon from '@mui/icons-material/Delete'
+import TableButton from '../../material-kit/buttons/LinkButton'
 
-function Termination() {
-  return (
-    <div>Termination</div>
-  )
+const Container = styled('div')(({ theme }) => ({
+    margin: '30px',
+    [theme.breakpoints.down('sm')]: {
+        margin: '16px',
+    },
+    '& .breadcrumb': {
+        marginBottom: '30px',
+        [theme.breakpoints.down('sm')]: {
+            marginBottom: '16px',
+        },
+    },
+}))
+
+const columns = [
+  { id: 'employee', label: 'Employee', minWidth: 10 },
+    { id: 'terminationDate', label: 'Termination Date', minWidth: 10 },
+    { id: 'description', label: 'Description ', minWidth: 10 },
+    { id: 'returnedItems', label: 'Returned Items', minWidth: 10 },
+]
+
+function createData(terminationDate, description, returnedItems,ApprovedDate,employee) {
+    return {
+        terminationDate: terminationDate,
+        description: description,
+        returnedItems: returnedItems,
+        employee:employee,
+    }
+}
+
+const rows = data.map((item, index) => {
+    const container = {}
+    createData(
+        (container.terminationDate = item.terminationDate),
+        (container.description = item.description),
+        (container.returnedItems = item.returnedItems),
+        (container.employee = item.employee),
+    )
+    return container
+})
+
+const StyledButton = styled(Button)(({ theme }) => ({
+    margin: theme.spacing(1),
+}))
+
+const Termination = () => {
+    const [show, setShow] = useState(false)
+
+    const handleButton = () => {
+        setShow(!show)
+        console.log(show)
+    }
+    return (
+        <Container>
+            <div className="breadcrumb">
+                <Breadcrumb
+                    routeSegments={[
+                        { name: 'Report', path: '/dashboard' },
+                        { name: 'Termination' },
+                    ]}
+                />
+            </div>
+            <Box py="1px" />
+            <Grid
+                container
+                direction="row"
+                spacing={2}
+                justifyContent="flex-end"
+            >
+            </Grid>
+            <Grid item xs={12}>
+                <SimpleCard title="Promotions">
+                    <PaginationTable columns={columns} rows={rows} />
+                </SimpleCard>
+            </Grid>
+        </Container>
+    )
 }
 
 export default Termination
