941bf0b3bffdaaa0a9b6af1dddabda712bca2b90
eze-une <mruniverse1x@gmail.com>
2022-06-20 12:10:42 +0300

    added missing UI

--- src/app/views/personalPage/myprofile/requests/Extended.jsx
+++ src/app/views/personalPage/myprofile/requests/Extended.jsx
@@ -1,9 +1,131 @@
 import React from 'react'
+import { Formik, Form } from 'formik'
+import * as Yup from 'yup'
+import {
+    Grid,
+    Typography,
+} from '@mui/material'
 
-function Extended() {
-  return (
-    <div>Extended</div>
-  )
+import Textfield from '../../../../components/FormsUI/Textfield'
+import DateTimePicker from '../../../../components/FormsUI/DataTimePicker'
+import Select from '../../../../components/FormsUI/Select'
+import Button from '../../../../components/FormsUI/Button/index'
+import itemlist from '../../../../utils/data/BranchList.json'
+import { Breadcrumb, SimpleCard } from 'app/components'
+import styled from '@emotion/styled'
+
+const buttonText = 'Save'
+
+const Container = styled('div')(({ theme }) => ({
+    margin: '30px',
+    [theme.breakpoints.down('sm')]: {
+        margin: '16px',
+    },
+    '& .breadcrumb': {
+        marginBottom: '30px',
+        [theme.breakpoints.down('sm')]: {
+            marginBottom: '16px',
+        },
+    },
+}))
+
+const INITIAL_FORM_STATE = {
+    vehicleName: '',
+    driverName: '',
+    FuelMeasurment: '',
+    AssignmentStatus: '',
+    TrackUsage: '',
+    StartDate: '',
+    EndDate: ''
 }
 
-export default Extended
+const FORM_VALIDATION = Yup.object().shape({
+    vehicleName: Yup.string().required('Required'),
+    driverName: Yup.string().required('Required'),
+    FuelMeasurment: Yup.string().required('Required'),
+    AssignmentStatus: Yup.string().required('Required'),
+    TrackUsage: Yup.string(),
+    StartDate: Yup.date().required('Required'),
+    EndDate: Yup.date().required('Required'),
+})
+
+function ExtendedLeaveRequest() {
+    return (
+        <Container>
+            <div className="breadcrumb">
+                <Breadcrumb
+                    routeSegments={[
+                        { name: 'Extended Leave Reuqest', path: '/dashboard' },
+                    ]}
+                />
+            </div>
+            <SimpleCard title="Extended Leave Request">
+                <Grid container>
+                    <Grid item xs={12}></Grid>
+                    <Grid item xs={12}>
+                        <Container maxWidth="md">
+                            <Formik
+                                initialValues={{
+                                    ...INITIAL_FORM_STATE,
+                                }}
+                                validationSchema={FORM_VALIDATION}
+                                onSubmit={(values) => {
+                                    console.log(values)
+                                }}
+                            >
+                                <Form>
+                                    <Grid container spacing={2}>
+                                        <Grid item xs={6}>
+                                            <Select
+                                                name="Employeeid"
+                                                label="Name"
+                                                options={itemlist}
+                                            />
+                                        </Grid>
+
+                                        <Grid item xs={6}>
+                                            <Select
+                                                name="department"
+                                                label="Leave Type"
+                                                options={itemlist}
+                                            />
+                                        </Grid>
+
+                                        <Grid item xs={6}>
+                                            <DateTimePicker
+                                                name="TripDate"
+                                                label="From Date "
+                                            />
+                                        </Grid>
+
+                                        <Grid item xs={6}>
+                                            <DateTimePicker
+                                                name="ToDate"
+                                                label="To Date "
+                                            />
+                                        </Grid>
+
+                                        <Grid item xs={6}>
+                                             <Textfield
+                                                name="description"
+                                                label="Description"
+                                                rows={4}
+                                            />
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Button>Submit Request</Button>
+                                        </Grid>
+                                    </Grid>
+                                </Form>
+                            </Formik>
+
+                            {/* </div> */}
+                        </Container>
+                    </Grid>
+                </Grid>
+            </SimpleCard>
+        </Container>
+    )
+}
+
+export default ExtendedLeaveRequest
