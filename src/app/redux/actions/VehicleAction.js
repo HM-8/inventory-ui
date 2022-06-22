74180d1ded8001ece7da96a5324bb97cf9f50dc3
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-06-22 13:30:31 +0300

    added notifications

--- src/app/redux/actions/VehicleAction.js
+++ src/app/redux/actions/VehicleAction.js
@@ -1,12 +1,22 @@
 import axios from 'axios'
 
 export const GET_VEHICLE_LIST = 'GET_VEHICLE_LIST'
+const accessToken = window.localStorage.getItem('accessToken')
+const AuthStr = `Bearer ${accessToken}`
 
 export const getVehicleList = () => (dispatch) => {
-    axios.get('http://localhost:4040/v1/Vehicle').then((res) => {
-        dispatch({
-            type: GET_VEHICLE_LIST,
-            payload: res.data,
+    axios
+        .get('http://localhost:4040/v1/vehicle', {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: AuthStr,
+            },
         })
-    })
+        .then((res) => {
+            dispatch({
+                type: GET_VEHICLE_LIST,
+                payload: res.data,
+            })
+        })
 }
