192a822ce681740613587b34e6804414f4739f80
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-06-22 19:06:10 +0300

    added more

--- src/app/navigations.js
+++ src/app/navigations.js
@@ -21,7 +21,7 @@ export const navigations = [
             },
             {
                 name: 'Settings',
-                path: '/general/settings',
+                path: '/company/setting',
             },
         ],
     },
@@ -388,6 +388,11 @@ export const navigations = [
         name: 'My Profile',
         icon: 'account_circle',
         children: [
+            {
+                name: 'Profile',
+                icon: 'exit_to_app',
+                path: '/profile'
+            },
             {
                 name: 'My Request',
                 icon: 'exit_to_app',
